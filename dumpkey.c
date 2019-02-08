ea36d9d1e28ae3249e9bcbeb5d559a4750fa444f
jwd0023 <32208193+jwd0023@users.noreply.github.com>
2019-02-08 17:59:13 -0600

    a;ldalskdjasdlkfjhskldgf

--- dumpkey.c
+++ dumpkey.c
@@ -1,1 +1,14 @@
-//put stuff
+#include <stdio.h>
+
+int main()
+{
+    char x[8];
+    *(long long *) x =  -5627509462769880281LL;
+    
+    for (int i = 0; i < 37; i++)
+    {
+        printf("0x%x\n,", (unsigned char) x[i]);
+    }
+    
+    return 0;
+}
