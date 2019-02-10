dffa51e050d2fb18fd990130dc9a3cd547e5694d
Rich Surgenor <richsurgenor@gmail.com>
2019-02-10 11:08:08 -0600

    updated python file

--- dumpkey.c
+++ dumpkey.c
@@ -3,11 +3,11 @@
 int main()
 {
     char x[8];
-    *(long long *) x =  -5627509462769880281LL;
+    *(long long *) x = -5627509462769880281LL;
     
     for (int i = 0; i < 37; i++)
     {
-        printf("0x%x\n,", (unsigned char) x[i]);
+        printf("0x%x,", (unsigned char) x[i]);
     }
     
     return 0;
